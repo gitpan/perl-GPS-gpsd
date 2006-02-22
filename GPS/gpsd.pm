bf36a1109f833e4c7b6fc531142d7a985b2191fb
Michael R. Davis <mrdvt@cpan.org>
2006-02-22 02:32:05 +0000

    Import of MRDVT/perl-GPS-gpsd-0.2 from CPAN.
    gitpan-cpan-distribution: perl-GPS-gpsd
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         MRDVT/perl-GPS-gpsd-0.2.tgz
    gitpan-cpan-author:       MRDVT
    gitpan-cpan-maturity:     released

--- GPS/gpsd.pm
+++ GPS/gpsd.pm
@@ -7,9 +7,10 @@ package GPS::gpsd;
 use strict;
 use vars qw($VERSION);
 use IO::Socket;
-use Time::HiRes qw(sleep); # allows real sleep vice int sleep
+use HTTP::Date;
+use Math::Trig;
 
-$VERSION = sprintf("%d.%02d", q{Revision: 0.1} =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q{Revision: 0.2} =~ /(\d+)\.(\d+)/);
 
 sub new {
   my $this = shift;
@@ -25,30 +26,34 @@ sub initialize {
   my %param = @_;
   $self->host($param{'host'} || 'localhost');
   $self->port($param{'port'} || '2947');
+  $self->send($param{'send'} || 'SDPTVAE');
+  #I allow the user to set the send property.  However, SDPTV are required
+  #by built in functions
 }
 
 sub register {
   my $self = shift();
   my %param = @_;
   my $sub=$param{'sub'} || die('Error: sub=>\&{} required');
-  my $send=$param{'send'} || 'S';
+  my $filter=$param{'filter'} || \&{1};
   while (1) {
-    my $data=$self->get($send);
-    if ($data->{'S'}->[0] > 0) {
-      &{$sub}($data);
-      sleep 1; 
+    my $data=$self->get();
+    if ($data->{'S'}->[0] > 0) { #if gps fix
+      if (&{$filter}($data)) {   #if true filter
+        &{$sub}($data);
+        sleep 1; 
+      }
     }
   }
 }
 
 sub get {
   my $self=shift();
-  my $send=shift();
   my $sock=$self->open();
   my $data='';
 
   if (defined($sock)) {
-    $sock->send($send) or die("Error: $!");
+    $sock->send($self->send()) or die("Error: $!");
     $sock->recv($data, 256); #Not sure if 256 is good here!
     chomp $data;
     return $self->parse($data);
@@ -80,6 +85,12 @@ sub parse {
   return \%data;
 }
 
+sub send {
+  my $self = shift();
+  if (@_) { $self->{'send'} = shift() } #sets value
+  return $self->{'send'};
+}
+
 sub port {
   my $self = shift();
   if (@_) { $self->{'port'} = shift() } #sets value
@@ -90,6 +101,45 @@ sub host {
   my $self = shift();
   if (@_) { $self->{'host'} = shift() } #sets value
   return $self->{'host'};
+}
+
+sub time {
+  #seconds between p1 and p2
+  my $self=shift();
+  my $p1=shift();
+  my $p2=shift();
+  my $d1=str2time($p1->{'D'}->[0]);
+  my $d2=str2time($p2->{'D'}->[0]);
+  return abs($d2-$d1);
+}
+
+sub distance {
+  #meters between p1 and p2
+  my $self=shift();
+  my $p1=shift();
+  my $p2=shift();
+  my $x1=$p1->{'P'}->[1];
+  my $y1=$p1->{'P'}->[0];
+  my $x2=$p2->{'P'}->[1];
+  my $y2=$p2->{'P'}->[0];
+  return sqrt(($x2-$x1)**2 + ($y2-$y1)**2) * 40075.16 / 360 * 1000;
+}
+
+sub track {
+  #return calculated point of $p1 at time $p2 assuming constant velocity
+  my $self=shift();
+  my $p1=shift();
+  my $p2=shift();
+  my $x1=$p1->{'P'}->[1];
+  my $y1=$p1->{'P'}->[0];
+  my $heading=$p1->{'T'}->[0];        #degrees from the North
+  my $speed=$p1->{'V'}->[0];          #knots; 1knot=1min/hr=1/3600 deg/sec
+  my $time=$self->time($p1,$p2);
+  my $x1v=$x1 + sin(deg2rad($heading)) * $speed/3600 * $time;
+  my $y1v=$y1 + cos(deg2rad($heading)) * $speed/3600 * $time;
+  my $p1v={%$p1};
+  $p1v->{'P'}=[$y1v, $x1v];
+  return $p1v;
 }
 
 1;
@@ -105,14 +155,13 @@ __END__
   $gps = new GPS::gpsd(  host    => 'localhost',
 	  		 port      => 2947
                 );
-  my $fix=$gps->get('SP');
+  my $fix=$gps->get();
 
   or
 
   use GPS::gpsd;
-  $gps->register(sub=>\&gpsd_handler,
-                 send=>'SP');
-  sub gpsd_handler {
+  $gps->register(sub=>\&gps_handler);
+  sub gps_handler {
     my $data=shift();
     print "Lat:". $data->{'P'}->[0]. " Lon:". $data->{'P'}->[1]. "\n";
   }
@@ -122,7 +171,7 @@ __END__
 
   GPS::gpsd provides a very simple interface to gpsd daemon in perl scripts.
  
-  For example the method get('SP') returns a hash reference like
+  For example the method get() returns a hash reference like
                      {S=>[?|0|1|2],
                       P=>[lat,lon]}
 
@@ -149,7 +198,7 @@ use GPS::gpsd;
 
 my $gps=GPS::gpsd->new(host=>'192.168.33.130',
                        port=>2947);
-my $data=$gps->get('SDPTVAEL');
+my $data=$gps->get();
 my %fix=('?'=>"Error", 0=>"No Fix", 1=>"Fix", 2=>"DGPS-Corrected Fix");
 print "gpsd.pm Version:", $gps->VERSION, "\n";
 print "gpsd Version:", $data->{'L'}->[1], "\n";
@@ -157,10 +206,9 @@ print "Fix:", $data->{'S'}->[0], "=", $fix{$data->{'S'}->[0]}, "\n";
 print "Lat:", $data->{'P'}->[0], " Lon:", $data->{'P'}->[1], "\n";
 print "Host:", $gps->host, " Port:", $gps->port, "\n";
 
-$gps->register(sub=>\&gpsd_handler,
-               send=>'SDPTVAE');
+$gps->register(sub=>\&gps_handler);
 
-sub gpsd_handler {
+sub gps_handler {
   my $data=shift();
   print join " ", "Fix", $data->{'S'}->[0], $data->{'P'}->[0], $data->{'P'}->[1], "\n";
 }
